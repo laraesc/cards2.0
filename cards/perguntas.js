66b41bc77b95746ee5818cbfa1001f20d098f7c9
laraesc <00001114277265sp@al.educacao.sp.gov.br>
2024-11-05 10:25:15 -0300

    Add files via upload

---
+++ cards/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Física',
+    'O que define uma onda?',
+    'Uma perturbação oscilante de alguma grandeza física no espaço e periódica no tempo'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Chile?',
+    'A capital do Chile é Santiago'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua estrangeira',
+    'Como se diz socorro em Árabe?',
+    'Socorro em Árabe é يساعد (yusaeidI)'
+)
